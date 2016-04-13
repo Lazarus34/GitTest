92b9dd19d13f4f3a6683f027120c1293fc9999e6
Andy Glover <andy.tank34@gmail.com>
2016-04-13 18:43:45 +0100

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by Andy Glover on 13/04/2016.
+//  Copyright © 2016 Andy Glover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
